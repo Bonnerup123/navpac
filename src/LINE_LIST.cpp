910811c53c081738e385012c4852f2cff327d900
Soren Bonnerup <sorenbonnerup@outlook.com>
2019-11-27 10:32:10 +0100

    First commit

---
+++ src/LINE_LIST.cpp
@@ -0,0 +1,110 @@
+
+ #include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+   
+
+   int main( int argc, char** argv )
+   {
+     ros::init(argc, argv, "points_and_lines");
+     ros::NodeHandle n;
+     ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+   
+     ros::Rate r(30);
+   
+     float f = 0.0;
+     while (ros::ok())
+     {
+   
+       visualization_msgs::Marker points, line_strip;
+       points.header.frame_id = line_strip.header.frame_id = "/map";
+       points.header.stamp = line_strip.header.stamp = ros::Time::now();
+       points.ns = line_strip.ns = "points_and_lines";
+       points.action = line_strip.action = visualization_msgs::Marker::ADD;
+       points.pose.orientation.w = line_strip.pose.orientation.w = 1.0;
+   
+   
+   
+       points.id = 0;
+       line_strip.id = 1;
+       
+   
+       points.type = visualization_msgs::Marker::POINTS;
+       line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+   
+       points.scale.x = 0.2;
+       points.scale.y = 0.2;
+  
+       line_strip.scale.x = 0.1;
+   
+   
+       points.color.g = 1.0f;
+       points.color.a = 1.0;
+   
+       line_strip.color.b = 1.0;
+       line_strip.color.a = 1.0;
+  
+   for (uint32_t i = 0; i < 1; ++i)
+       {
+        geometry_msgs::Point p;
+        p.x = -3;
+        p.y = -2;
+        p.z = 0;
+  
+        points.points.push_back(p);
+        line_strip.points.push_back(p);
+   
+       }
+   for (uint32_t i = 0; i < 1; ++i)
+       {
+        geometry_msgs::Point p;
+        p.x = 5;
+        p.y = -2;
+        p.z = 0;
+  
+        points.points.push_back(p);
+        line_strip.points.push_back(p);
+   
+       }
+   for (uint32_t i = 0; i < 1; ++i)
+       {
+        geometry_msgs::Point p;
+        p.x = 5;
+        p.y = -3;
+        p.z = 0;
+  
+        points.points.push_back(p);
+        line_strip.points.push_back(p);
+   
+       }
+   for (uint32_t i = 0; i < 1; ++i)
+       {
+        geometry_msgs::Point p;
+        p.x = -3;
+        p.y = -3;
+        p.z = 0;
+  
+        points.points.push_back(p);
+        line_strip.points.push_back(p);
+   
+       }
+   for (uint32_t i = 0; i < 1; ++i)
+       {
+        geometry_msgs::Point p;
+        p.x = -3;
+        p.y = -2;
+        p.z = 0;
+  
+        points.points.push_back(p);
+        line_strip.points.push_back(p);
+   
+       }
+
+
+      marker_pub.publish(points);
+      marker_pub.publish(line_strip);
+  
+      r.sleep();
+  
+      f += 0.04;
+    }
+  }
---
+++ src/nav.cpp
@@ -0,0 +1,155 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <kobuki_msgs/AutoDockingAction.h>
+#include <geometry_msgs/Twist.h>
+#include <actionlib/client/simple_client_goal_state.h>
+
+bool moveToGoal(double xGoal, double yGoal);
+void docking();
+void learnpos();
+
+double x1 = 5.0;
+double py1 = -2.0;
+double x2 = 5.0;
+double y2 = -3.0;
+double x3 = -3.0;
+double y3 = -3.0;
+double x4 = -3.0;
+double y4 = -2.0;
+
+bool goalReached = false;
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "navigation");
+	ros::NodeHandle n;
+
+	learnpos();
+    docking();
+	ros::spinOnce();
+	
+
+	char choice = 'y';
+	while (choice == 'y')
+	{
+		
+		if (choice == 'y'){
+          
+			goalReached = moveToGoal(x1, py1);
+
+			goalReached = moveToGoal(x2, y2);
+				
+			goalReached = moveToGoal(x3, y3);
+			
+			goalReached = moveToGoal(x4, y4);	
+			
+				
+		
+		if (goalReached == true){
+				ROS_INFO("Route completed\n");
+				ros::spinOnce();
+				ros::spinOnce();
+
+			}else{
+				ROS_INFO("unable to comply! please shut down and reset\n");
+			}
+		}
+		std::cout << "repeat route? y/n\n";
+		std::cin >> choice;
+	}
+	
+	return 0;
+}
+
+bool moveToGoal(double xGoal, double yGoal){
+
+	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
+	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
+
+	move_base_msgs::MoveBaseGoal goal;
+
+	goal.target_pose.header.frame_id = "/map";
+	goal.target_pose.header.stamp = ros::Time::now();
+
+	goal.target_pose.pose.position.x =  xGoal;
+	goal.target_pose.pose.position.y =  yGoal;
+	goal.target_pose.pose.position.z =  0.0;
+	goal.target_pose.pose.orientation.x = 0.0;
+	goal.target_pose.pose.orientation.y = 0.0;
+	goal.target_pose.pose.orientation.z = 0.0;
+	goal.target_pose.pose.orientation.w = 1.0;
+
+	ROS_INFO("Sending goal location ...");
+	ac.sendGoal(goal);
+
+	ac.waitForResult();
+
+	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+		ROS_INFO("You have reached a turning point");
+		return true;
+	}
+	else{
+		ROS_INFO("The robot failed to reach the destination");
+		return false;
+	}
+
+}
+
+void docking(){
+	 // Create the client
+  actionlib::SimpleActionClient<kobuki_msgs::AutoDockingAction> docking_ac("dock_drive_action", true);
+  docking_ac.waitForServer();
+
+  // Create goal object
+  kobuki_msgs::AutoDockingGoal goal;
+
+  // Assign initial state
+  actionlib::SimpleClientGoalState dock_state = actionlib::SimpleClientGoalState::LOST;
+
+  // Send the goal
+  docking_ac.sendGoal(goal);
+
+  ros::Time time = ros::Time::now();
+
+  // Monitor progress
+  while (!docking_ac.waitForResult(ros::Duration(3))) {
+
+    dock_state = docking_ac.getState();
+    ROS_INFO("Docking status: %s",dock_state.toString().c_str());
+
+    if (ros::Time::now() > (time+ros::Duration(20000))) {
+      ROS_INFO("Docking took more than 20 seconds, canceling.");
+      docking_ac.cancelGoal();
+      break;
+    }// end if
+  }// end while
+}
+
+void learnpos(){
+
+    ros::NodeHandle r;
+    ros::Publisher cmd_vel_pub = r.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop",1);
+
+    geometry_msgs::Twist cmd_vel_message;
+    cmd_vel_message.angular.z = 0.0;
+    cmd_vel_message.linear.x = 0.5;
+    ros::Rate loop_rate(10);
+    for(int i = 0; i < 1; i++)
+    {
+        cmd_vel_pub.publish(cmd_vel_message);
+        ros::Duration(2).sleep();
+        
+    }
+
+    cmd_vel_message.angular.z = 0.5;
+    cmd_vel_message.linear.x = 0.0;
+    ros::Duration(10).sleep();
+    for(int i = 0; i < 35; i++)
+    {
+        cmd_vel_pub.publish(cmd_vel_message);
+        ros::Duration(0.5).sleep();
+        
+    }
+}
