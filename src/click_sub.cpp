0728f3e2b24b18147a71c75b94d13058fc921201
xamza1608 <xamza1608@live.dk>
2019-12-07 22:41:03 +0100

    Update on the userinterface on click_sub.cpp,typos

--- src/click_sub.cpp
+++ src/click_sub.cpp
@@ -3,24 +3,28 @@
 #include <sstream>
 #include "std_msgs/String.h"
 
+int x=1;
 
 void msgCallback(const geometry_msgs::PointStamped::ConstPtr& msg){
-    
-    ROS_INFO("The coordinantes you have clicked on is: %f, %f, %f", msg->point.x, msg->point.y, msg->point.z);
+    std::cout<<"This was point "<<x<<".\n";
+    ROS_INFO("Clicked point is: \x1B[92m%f\033[0m, \x1B[94m%f\033[0m\n\n" , msg->point.x, msg->point.y);
+    x++; 
+ 
+
 }
 
 
 
 int main(int argc, char **argv){
 
-    std::cout<<"how to use. \n1:Publish a point. \n2: See the published point on the terminal. \n\n\n3: Profit????? \n\n\n\n\n-----------------"<<std::endl;
+    std::cout<<"\e[1mCOORDINATE FINDER\e[0m \nHow to use: \n1:Publish a point. \n2:See the published point on the terminal. \n\n-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴-̴"<<std::endl;
 
   
     ros::init(argc, argv, "click_sub");
 
     ros::NodeHandle n;
-
-    ros::Subscriber sub = n.subscribe("clicked_point", 1000, msgCallback);    
+       
+    ros::Subscriber sub = n.subscribe("clicked_point", 1000, msgCallback);
 
     ros::spin();
 
