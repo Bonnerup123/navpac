681d25edcfa776bb3cef84b5264c04f5211d5841
kasper <kvni19@student.aau.dk>
2019-12-08 14:17:49 +0100

    nu med kommentarer på nav koden

--- src/nav.cpp
+++ src/nav.cpp
@@ -5,101 +5,81 @@
 #include <geometry_msgs/Twist.h>
 #include <actionlib/client/simple_client_goal_state.h>
 
-bool moveToGoal(double xGoal, double yGoal);
+bool moveToGoal(double xGoal, double yGoal); //funktionsdefinationer til senere i koden
 void docking();
 void learnpos();
 
-//punkter paa ruten
-double x1 = -6.35;
-double py1 = -13.43;
-double x2 = 0.0;
-double y2 = 0.0;
-double x3 = 0.0;
-double y3 = 0.0;
-double x4 = 0.0;
-double y4 = 0.0;
+//punkter paa ruten udtryk ved en vektor, så der er mulighed for at skrive koordinater på (x, y) form. gemt i en double som indeholder 8 bytes.
+double p1[2] = {0, 0};
+double p2[2] = {0, 0};
+double p3[2] = {0, 0};
+double p4[2] = {0, 0};
 
-bool goalReached = false;
+int main(int argc, char** argv){                                                    //mainfunktion
+	ros::init(argc, argv, "navigation");                                            //stárt noden
+	ros::NodeHandle n;                                                              //nodehandle til at kommunikere med masteren
 
-int main(int argc, char** argv){
-	ros::init(argc, argv, "navigation");
-	ros::NodeHandle n;
-
-	learnpos();
+	learnpos();                                                                     //starter learnpos funktionen som er beskrevet længere nede
     
-	ros::spinOnce();
+	ros::spinOnce();                                                                //ros funktion som er vital for loop funktioner i ros loops
 	
 
-	char choice = 'y';
-	while (choice == 'y')
+	char choice = 'y';                                                              //vi laver en char(1 byte) med startværdien y
+	while (choice == 'y')                                                           //loop som kører, hvis choice er y
 	{
 		
-		if (choice == 'y'){
+		if (choice == 'y'){                                                         //et if statement som også kun kører hvis choice er y.
         
-			 goalReached = moveToGoal(x1, py1);
-        
-	
-				goalReached = moveToGoal(x2, y2);
-	
+moveToGoal(p1[0], p1[1]);                                                           //funktion som sender vektorkoordinaterne i 1. og 2. indgang til movetogoal
+
+moveToGoal(p2[0], p2[1]);	
 			
 
-				goalReached = moveToGoal(x3, y3);	
+moveToGoal(p3[0], p3[1]);	
 	
 			
-				goalReached = moveToGoal(x4, y4);		
+moveToGoal(p4[0], p4[1]);		
 		
 			
-				
-			
-				
-		
-		if (goalReached == true){
-				ROS_INFO("Route completed\n");
-				ros::spinOnce();
-				ros::spinOnce();
-
-			}else{
-				ROS_INFO("unable to comply! please shut down and reset\n");
-			}
-		}
-		std::cout << "repeat route? y/n\n";
+}                                                                                   // afslutning af if statement
+		std::cout << "repeat route? y/n\n";                                         // et cout, cin spørgsmål om ruten skal køre, ændres choice til alt andet end y, vil while loopet afslutte
 		std::cin >> choice;
 	}
-	docking();
-	return 0;
+	docking();                                                                      // kalder dockingfunktionen som ikke virker
+	return 0;                                                                       // afslutter programmet.
 }
 
-bool moveToGoal(double xGoal, double yGoal){
+bool moveToGoal(double xGoal, double yGoal){                                        // funktionsnavn og defination af de to værdier den skal have med fra main funktionen
 
-	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
-	while(!ac.waitForServer(ros::Duration(5.0))){
+	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true); // starter en actionclient som skal flytte robotten
+	while(!ac.waitForServer(ros::Duration(5.0))){                                         // standardbesked hvis ikke noden kan få kontakt til actionserveren, den får 5 sekunder 
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
 
-	move_base_msgs::MoveBaseGoal goal;
+	move_base_msgs::MoveBaseGoal goal;                                                   // actionbeskeden defineres som typen Goal, fra move_base_msgs som er et bibleotek af beskeder, der hører med til move base funktionen
 
-	goal.target_pose.header.frame_id = "/map";
-	goal.target_pose.header.stamp = ros::Time::now();
+	goal.target_pose.header.frame_id = "/map";                                           //frame id, er rammen for den ønskede bevægelse
+	goal.target_pose.header.stamp = ros::Time::now();                                    //ros tid, bruges til tidssync for funktionerne
 
-	goal.target_pose.pose.position.x =  xGoal;
+	goal.target_pose.pose.position.x =  xGoal;                                           // her indsættes de to koordinater fra main funktionen som x og y koordinat. z er altid 0, da vi arbejder i 2d
 	goal.target_pose.pose.position.y =  yGoal;
 	goal.target_pose.pose.position.z =  0.0;
-	goal.target_pose.pose.orientation.x = 0.0;
-	goal.target_pose.pose.orientation.y = 0.0;
+	goal.target_pose.pose.orientation.x = 0.0;                                           //her sættes målets orientering, og der sendes ingen x,y,z dimensioner, da dette er fuldstændig ligegyldigt for vores mål.
+	goal.target_pose.pose.orientation.y = 0.0;                                           // til gengæld sættes w til 1.0 for at definere at vores mål eksiterer, en orientering på 0.0.0.0 ville gøre at det var et 0-dimensionelt mål hvilket er svært at køre efter
 	goal.target_pose.pose.orientation.z = 0.0;
 	goal.target_pose.pose.orientation.w = 1.0;
 
-	ROS_INFO("Sending goal location ...");
-	ac.sendGoal(goal);
+	ROS_INFO("Sending goal location ...");                                               //her sendes en besked når der er sendt en target pose
+	ac.sendGoal(goal);                                                                   //her sendes goal beskeden med alle informationerne i til actionserveren. noter at sendgoal() er en funktion
 
-	ac.waitForResult();
+	ac.waitForResult();                                                                  //ac skal vente på at serveren løser opgaven også en funktion
 
-	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){                    //et if statement der printer en besked hvis funktionen getstate() sender værdien succeded retur
 		ROS_INFO("You have reached a turning point");
-		return true;
+		return true;                                                                     //returnerer true fra funktionen.
 	}
 	else{
-		ROS_INFO("The robot failed to reach the destination");
+		ROS_INFO("The robot failed to reach the destination");                           //returnerer false hvis ikke robotten kan gennemføre
 		return false;
 	}
 
@@ -135,29 +115,18 @@ void docking(){
   }// end while
 }
 
-void learnpos(){
+void learnpos(){                                                                  //funktionsnavn. void betyder at den ikke returnerer noget.
 
-    ros::NodeHandle r;
-    ros::Publisher cmd_vel_pub = r.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop",1);
+    ros::NodeHandle r;                                                            //nodehandle til funktionen, er nok egentlig ikke nødvendigt, men hey.. det virker
+    ros::Publisher cmd_vel_pub = r.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop",1); //opretter en publisher som skriver beskeder på teleop topic'et
 
-    geometry_msgs::Twist cmd_vel_message;
-    cmd_vel_message.angular.z = 0.0;
-    cmd_vel_message.linear.x = 0.5;
-    ros::Rate loop_rate(10);
-    for(int i = 0; i < 1; i++)
+    geometry_msgs::Twist cmd_vel_message;                                         //vi bruger class'en cmd_vel_messege i beskeden
+    cmd_vel_message.angular.z = 0.5;                                              //vi giver den ene værdi i beskeden som 0.5 m/s rotation om z aksen
+    ros::Duration(10).sleep();                                                    // en ros duration vi skal prøve at køre uden..
+    for(int i = 0; i < 35; i++)                                                   // et for-loop der kører 35 omgange, det er beregnet ved "baglæns korrektion"
     {
-        cmd_vel_pub.publish(cmd_vel_message);
-        ros::Duration(2).sleep();
-        
-    }
-
-    cmd_vel_message.angular.z = 0.5;
-    cmd_vel_message.linear.x = 0.0;
-    ros::Duration(10).sleep();
-    for(int i = 0; i < 35; i++)
-    {
-        cmd_vel_pub.publish(cmd_vel_message);
-        ros::Duration(0.5).sleep();
+        cmd_vel_pub.publish(cmd_vel_message);                                     //publish beskeden om den angulære accelleration.
+        ros::Duration(0.5).sleep();                                               // en besked om at køre beskeden i kun et halvt sekund. Dette gør vi fordi cmd_vel_messege kører i et helt sekund, og for at undgå hak i runden, skal loopet starte igen, så der er en ny besked til robotten.
         
     }
 }
